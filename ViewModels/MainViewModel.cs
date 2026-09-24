df2e02c9829850ba09323719542d221ed66da0b6
Oskar <skerret5@gmail.com>
2026-09-24 09:07:18 +0200

    fix(error)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -5,5 +5,5 @@ namespace budujemyoknorecznie.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    private string greeting = "Welcome to Avalonia!";
 }
